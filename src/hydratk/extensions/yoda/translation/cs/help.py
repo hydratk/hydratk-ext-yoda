10b3b8c84b85a90c61626a3e78829b5d3dbfe9b5
Petr Rašek <bowman@hydratk.org>
2016-11-03 21:26:00 +0100

    Fixed - removed option test-repo-root-dir for command create-test-results-db

--- src/hydratk/extensions/yoda/translation/cs/help.py
+++ src/hydratk/extensions/yoda/translation/cs/help.py
@@ -31,7 +31,7 @@ help_cmd = {
 help_opt = {
    'yoda-test-path' : { '{h}--yoda-test-path <cesta>{e}' : { 'description' : 'cesta k testovacímu scénáři', 'commands' : ('yoda-run','yoda-simul')}},
    'yoda-test-run-name' : { '{h}--yoda-test-run-name <název>{e}' : { 'description' : 'identifikátor spouštěného běhu', 'commands' : ('yoda-run','yoda-simul')}},
-   'yoda-test-repo-root-dir' : { '{h}--yoda-test-repo-root-dir <cesta>{e}' : { 'description' : 'cesta ke kořenovému adresáři test. repozitáře', 'commands' : ('yoda-run','yoda-simul','yoda-create-test-results-db')}},
+   'yoda-test-repo-root-dir' : { '{h}--yoda-test-repo-root-dir <cesta>{e}' : { 'description' : 'cesta ke kořenovému adresáři test. repozitáře', 'commands' : ('yoda-run','yoda-simul')}},
    'yoda-test-results-output-create' : { '{h}--yoda-test-results-output-create <stav>{e}' : { 'description' : 'aktivuje/deaktivuje nativní zpracování výsledků testů', 'commands' : ('yoda-run','yoda-simul')}},
    'yoda-test-results-output-handler' : { '{h}--yoda-test-results-output-handler <typ>{e}' : { 'description' : 'nastaví typ handleru pro zpracování výsledků testů', 'commands' : ('yoda-run','yoda-simul')}},
    'yoda-db-results-dsn' : { '{h}--yoda-db-results-dsn <dsn>{e}' : { 'description' : 'definice přístupu k databázi výsledků testů', 'commands' : ('yoda-run','yoda-simul','yoda-create-test-results-db')}},
@@ -39,9 +39,9 @@ help_opt = {
    #standalone with option profile yoda 
    'test-path' : { '{h}-tp, --test-path <cesta>{e}' : { 'description' : 'cesta k testovacímu scénáři', 'commands' : ('run','simul')}},
    'test-run-name' : { '{h}-tn, --test-run-name <název>{e}' : { 'description' : 'identifikátor spouštěného běhu', 'commands' : ('run','simul')}},
-   'test-repo-root-dir' : { '{h}-tr, --test-repo-root-dir <cesta>{e}' : { 'description' : 'cesta ke kořenovému adresáři test. repozitáře', 'commands' : ('run','simul','create-test-results-db')}},
+   'test-repo-root-dir' : { '{h}-tr, --test-repo-root-dir <cesta>{e}' : { 'description' : 'cesta ke kořenovému adresáři test. repozitáře', 'commands' : ('run','simul')}},
    'test-results-output-create' : { '{h}-oc, --test-results-output-create <stav>{e}' : { 'description' : 'aktivuje/deaktivuje nativní zpracování výsledků testů', 'commands' : ('run','simul')}},
    'test-results-output-handler' : { '{h}-oh, --test-results-output-handler <typ>{e}' : { 'description' : 'nastaví typ handleru pro zpracování výsledků testů', 'commands' : ('run','simul')}},
    'db-results-dsn' : { '{h}--db-results-dsn <dsn>{e}' : { 'description' : 'definice přístupu k databázi výsledků testů', 'commands' : ('run','simul','create-test-results-db')}},
-   'db-testdata-dsn' : { '{h}--db-testdata-dsn <dsn>{e}' : { 'description' : 'definice přístupu k databázi testovacích dat', 'commands' : ('yoda-create-testdata-db')}}      
+   'db-testdata-dsn' : { '{h}--db-testdata-dsn <dsn>{e}' : { 'description' : 'definice přístupu k databázi testovacích dat', 'commands' : ('create-testdata-db')}}      
 }
--- src/hydratk/extensions/yoda/translation/en/help.py
+++ src/hydratk/extensions/yoda/translation/en/help.py
@@ -31,7 +31,7 @@ help_cmd = {
 help_opt = {
    'yoda-test-path' : { '{h}--yoda-test-path <path>{e}' : { 'description' : 'test scenario path', 'commands' : ('yoda-run','yoda-simul')}},
    'yoda-test-run-name' : { '{h}--yoda-test-run-name <name>{e}' : { 'description' : 'test run identification', 'commands' : ('yoda-run','yoda-simul')}},
-   'yoda-test-repo-root-dir' : { '{h}--yoda-test-repo-root-dir <path>{e}' : { 'description' : 'test repository root directory', 'commands' : ('yoda-run','yoda-simul','yoda-create-test-results-db')}},
+   'yoda-test-repo-root-dir' : { '{h}--yoda-test-repo-root-dir <path>{e}' : { 'description' : 'test repository root directory', 'commands' : ('yoda-run','yoda-simul')}},
    'yoda-test-results-output-create' : { '{h}--yoda-test-results-output-create <state>{e}' : { 'description' : 'activates/deactivates native test results output handler', 'commands' : ('yoda-run','yoda-simul')}},
    'yoda-test-results-output-handler' : { '{h}-a, --yoda-test-results-output-handler <type>{e}' : { 'description' : 'set the test results output handler type', 'commands' : ('yoda-run','yoda-simul')}},   
    'yoda-db-results-dsn' : { '{h}--yoda-db-results-dsn <dsn>{e}' : { 'description' : 'test results database access definition', 'commands' : ('yoda-run','yoda-simul','yoda-create-test-results-db')}},
@@ -39,7 +39,7 @@ help_opt = {
    #standalone with option profile yoda
    'test-path' : { '{h}-tp, --test-path <path>{e}' : { 'description' : 'test scenario path', 'commands' : ('run','simul')}},
    'test-run-name' : { '{h}-tn, --test-run-name <name>{e}' : { 'description' : 'test run identification', 'commands' : ('run','simul')}},
-   'test-repo-root-dir' : { '{h}-tr, --test-repo-root-dir <path>{e}' : { 'description' : 'test repository root directory', 'commands' : ('run','simul','create-test-results-db')}},
+   'test-repo-root-dir' : { '{h}-tr, --test-repo-root-dir <path>{e}' : { 'description' : 'test repository root directory', 'commands' : ('run','simul')}},
    'test-results-output-create' : { '{h}-oc, --test-results-output-create <state>{e}' : { 'description' : 'activates/deactivates native test results output handler', 'commands' : ('run','simul')}},
    'test-results-output-handler' : { '{h}-oh, --test-results-output-handler <type>{e}' : { 'description' : 'set the test results output handler type', 'commands' : ('run','simul')}},
    'db-results-dsn' : { '{h}--db-results-dsn <dsn>{e}' : { 'description' : 'test results database access definition', 'commands' : ('run','simul','create-test-results-db')}},  
